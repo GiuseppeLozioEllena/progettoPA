ee883fe13e8e4973edf72c139607815e2d8cc338
Manuel <manuelp92@hotmail.it>
2016-10-05 11:45:04 +0200

    Aggiunta funzione per caricare modello navicella2

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ $(function()
 	   camera.lookAt(scene.position);
 	   
 	container = document.getElementById("webGL-container");
-	
   controls = new THREE.FlyControls( camera );
 	controls.movementSpeed = 1000;
 	controls.domElement = container;
@@ -43,6 +42,7 @@ $(function()
   spotLight.position.set(20,30,40);
   scene.add(spotLight);
   
+  //caricaNavicella();
   for (i = 0; i < 20; i++)
   {
 	x = getRandomInt(-30,30); 
@@ -80,7 +80,39 @@ $(function()
               child.material.map = texture;
             }
 		  });
+		object.position.set(0,0,0);
+        scene.add( object );
+        },onError );
+  }
+  
+  function caricaNavicella()
+  {
+        var manager = new THREE.LoadingManager();
+        var texture = new THREE.Texture();
+
+        var onError = function ( xhr ) {
+        };
+
+        var loader = new THREE.ImageLoader( manager );
+        loader.load( 'model/texture_navicella_blu.jpg', function ( image ) {
+          texture.image = image;
+          texture.needsUpdate = true;
+        } );
+		
+        // model
+        var loader = new THREE.OBJLoader( manager );
+        loader.load( 'model/navicella.obj', function ( object )
+        {
+          object.traverse( function ( child ) 
+          {
+            if ( child instanceof THREE.Mesh ) 
+            {
+              child.material.map = texture;
+			  child.scale.set( 0.01, 0.01, 0.01 );
+            }
+		  });
 		object.position.set(x,y,z);
+		
         scene.add( object );
         },onError );
   }
@@ -110,6 +142,7 @@ $(function()
 		requestAnimationFrame(animate);
 		stats.update();
    		renderer.render(scene,camera);
+		
 		d = 2;
 		controls.movementSpeed = 0.33 * d;
 		controls.update( delta );
@@ -124,6 +157,5 @@ $(function()
    		camera.aspect=SCREEN_WIDTH/SCREEN_HEIGHT;
    		camera.updateProjectionMatrix();
    		renderer.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
-
    });
 });
